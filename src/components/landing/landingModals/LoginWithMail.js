b7fe82c7e46eefb52367ba36929608da5ccc3cd9
sanjay1151 <35390831+sanjay1151@users.noreply.github.com>
2020-10-05 14:43:49 +0530

    Update LoginWithMail.js

---
+++ src/components/landing/landingModals/LoginWithMail.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const LoginWithMail = () => {
+  const [open, setOpen] = useState(false);
+
+  return <h3>Login with Mail, or Phone Number with Password</h3>;
+};
+
+export default LoginWithMail;
